20b5a009e476699b155673647719c538c435993c
NatashaOvdina <natashaovdina@yahoo.com>
2024-02-05 21:20:25 +0300

    Still trying

--- Inspiration/Services/NetworkManager.swift
+++ Inspiration/Services/NetworkManager.swift
@@ -12,25 +12,6 @@ final class NetworkManager {
     static let shared = NetworkManager()
  
     private init() {}
+  
     
-    func fetchQuote() {
-        AF.request(Link.randomQuoteURL.url)
-            .validate()
-            .responseJSON { [unowned self]dataResponse in
-                switch dataResponse.result {
-                case .success(let value):
-                    guard let randomQuoteInfo = value as? [String:Any] else {return}
-                    
-                        let quoteInfo = Quote(
-                            quoteText: randomQuoteInfo["quoteText"] as? String ?? "",
-                            quoteAuthor: randomQuoteInfo["quoteAuthor"] as? String ?? "",
-                            quoteGenre: randomQuoteInfo["quoteGenre"] as? String ?? "")
-                        
-                
-                    
-                case .failure(let error):
-                    print(error)
-                }
-            }
-    }
 }
--- Inspiration/ViewControllers/MainViewController.swift
+++ Inspiration/ViewControllers/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 final class MainViewController: UIViewController {
     
@@ -31,8 +32,30 @@ final class MainViewController: UIViewController {
     }
     
     private func fetchRandomQuote() {
-        
-        
-        
+        AF.request(Link.randomQuoteURL.url)
+            .validate()
+            .responseJSON { [weak self] dataResponse in
+                guard let self else { return }
+                switch dataResponse.result {
+                case .success(let value):
+                    guard let quoteInfo = value as? [ String: Any ] else { return }
+                    
+                    let quote = Quote(
+                        quoteText: quoteInfo["quoteText"] as? String ?? "",
+                        quoteAuthor: quoteInfo["quoteAuthor"] as? String ?? "",
+                        quoteGenre: quoteInfo["quoteGenre"] as? String ?? ""
+                    )
+                    
+                    let info = Info(data: [quote])
+                    
+                    quoteLabel.text = info.data.first?.quoteText
+                    authorLabel.text = info.data.first?.quoteAuthor
+                    genreLabel.text = info.data.first?.quoteGenre
+                    activityIndicator.stopAnimating()
+                    
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 }
